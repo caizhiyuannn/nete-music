6fb3b0d11e44a97024e4cc5dc55b5918238d8945
caizhiyuannn <caizhiyuannn@gmail.com>
2020-07-14 15:54:52 +0800

    add carousel init

---
+++ frontend/src/components/carousel/item.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="carousel-item">
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nete-carouse-item',
+};
+</script>
+
+<style></style>
---
+++ frontend/src/components/carousel/main.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="carousel">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nete-carousel',
+  props: {
+    itemWidth: {
+      type: String,
+      default: '540px',
+    },
+    itemHeight: {
+      type: String,
+      default: '200px',
+    },
+    defaultIndex: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      items: [],
+      activeIndex: -1,
+    };
+  },
+  methods: {
+    calcItems() {
+      this.items = this.$children.filter(
+        (child) => child.$options.name === 'nete-carouse-item'
+      );
+    },
+    pre() {},
+    next() {},
+  },
+};
+</script>
+<style lang="scss" scoped></style>
