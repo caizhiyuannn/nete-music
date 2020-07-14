9d7988c5d8b49dd30ec1e176a3278fac0b50777e
caizhiyuannn <caizhiyuannn@gmail.com>
2020-07-14 16:36:30 +0800

    add newsong

--- frontend/src/api/discovery.js
+++ frontend/src/api/discovery.js
@@ -6,3 +6,6 @@ export const getPersonalized = () => request.get('/personalized?limit=10');
 
 export const getPrivateContent = () =>
 request.get('/personalized/privatecontent/list?limit=4');
+
+
+export const getNewSong = () => request.get('/personalized/newsong')
---
+++ frontend/src/views/music/discovery/newsong.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="newsong">
+    <h3 style="padding: 0 8px">最新音乐</h3>
+    <div class="card-container">
+      <div class="card-item" v-for="(song, idx) in songs" :key="idx">
+        <div class="avator">
+          <img :src="song.picUrl" />
+        </div>
+        <div class="top">{{ idx < 9 ? `0${idx + 1}` : idx + 1 }}</div>
+        <div class="info">
+          <span>{{ song.name }}</span>
+          <span>{{ song.song.artists[0].name }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getNewSong } from '@/api';
+export default {
+  name: 'nete-newsong',
+  data() {
+    return {
+      songs: [],
+    };
+  },
+  async created() {
+    const { result } = await getNewSong();
+    this.songs = result;
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card-container {
+  display: flex;
+  flex-direction: column;
+  height: 400px;
+  width: 100%;
+  flex-wrap: wrap;
+
+  .card-item {
+    width: 50%;
+    height: 80px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    cursor: pointer;
+    padding: 8px;
+    &:hover {
+      background-color: #f5f5f5;
+    }
+  }
+  .avator img {
+    width: 60px;
+    display: block;
+    border-radius: 6px;
+  }
+
+  .top {
+    padding: 0 8px;
+  }
+  .info span {
+    display: block;
+    &:last-child {
+      color: #676767;
+    }
+  }
+}
+</style>
