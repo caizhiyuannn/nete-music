0c6149a682624ff1bf66992c51bfb5058a5ddb0a
caizhiyuannn <caizhiyuannn@gmail.com>
2020-07-15 00:06:28 +0800

    add backdrop-filter blur

--- frontend/src/views/music/discovery/newsong.vue
+++ frontend/src/views/music/discovery/newsong.vue
@@ -5,6 +5,10 @@
       <div class="card-item" v-for="(song, idx) in songs" :key="idx">
         <div class="avator">
           <img :src="song.picUrl" />
+
+          <span class="material-icons">
+            play_arrow
+          </span>
         </div>
         <div class="top">{{ idx < 9 ? `0${idx + 1}` : idx + 1 }}</div>
         <div class="info">
@@ -46,16 +50,51 @@ export default {
     display: flex;
     flex-direction: row;
     align-items: center;
-    cursor: pointer;
     padding: 8px;
     &:hover {
       background-color: #f5f5f5;
+      border-radius: 4px;
     }
   }
+
   .avator img {
+    position: absolute;
     width: 60px;
     display: block;
     border-radius: 3px;
+    cursor: pointer;
+  }
+
+  .avator {
+    width: 60px;
+    height: 100%;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .avator span {
+    color: #d13c37;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 26px;
+    height: 26px;
+    position: absolute;
+    z-index: 1;
+    cursor: pointer;
+    &::after {
+      content: '';
+      position: absolute;
+      width: 26px;
+      height: 26px;
+      background-color: rgba($color: #fff, $alpha: 0.3);
+      backdrop-filter: blur(3px);
+      -webkit-backdrop-filter: blur(3px);
+      z-index: -1;
+      border-radius: 50%;
+    }
   }
 
   .top {
@@ -63,8 +102,16 @@ export default {
   }
   .info span {
     display: block;
+    user-select: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+
     &:last-child {
+      cursor: pointer;
       color: #676767;
+      &:hover {
+        color: #333333;
+      }
     }
   }
 }
